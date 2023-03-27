9af079f0ed82aa0e7aa36a5fc1cbd0354ba1ed51
ThereWolf <bdunham484@gmail.com>
2023-03-27 13:56:49 -0500

    complete activity 03

--- 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
+++ 05-Third-Party-APIs/01-Activities/06-Stu_Form-Elements/Unsolved/assets/js/script.js
@@ -1,6 +1,15 @@
 var shoppingFormEl = $('#shopping-form');
 var shoppingListEl = $('#shopping-list');
+// add variabeles for HTML elements
+var inputEl = $('#shopping-input');
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
+function handleFormSubmit(event) {
+    event.preventDefault();
 
+    console.log('Form Entry: ' + inputEl.val());
+    shoppingListEl.append('<li>' + inputEl.val() + '</li>');
+    $('input[type="text"]').val("");
+}
 // TODO: Add an event listener to the `shoppingFormEl` to handle submission
+shoppingFormEl.on('submit', handleFormSubmit);
