f094086c13ae881c316f16c1ca93f12d7c92f711
ThereWolf <bdunham484@gmail.com>
2023-03-29 18:50:25 -0500

    preclasswork

--- 05-Third-Party-APIs/04-preclass-drill/wednesday/script.js
+++ 05-Third-Party-APIs/04-preclass-drill/wednesday/script.js
@@ -1,16 +1,33 @@
-const vowelCounter = (string) => {
-    let vowelArr = ['a', 'e', 'i', 'o', 'u'];
-    let vowelsInStringArr = [];
+let buttonEl = document.getElementById('form-submit');
+let inputEl = document.getElementById('form-input');
+let responseEl = document.getElementById('response');
 
-    for (let i = 0; i < string.length; i++) {
-        for (let n = 0; n < vowelArr.length; n++) {
-            if (string[i] === vowelArr[n]) {
-                vowelsInStringArr.push(string[i])
-                console.log(string[i] + ' is a vowel')
+
+const clickHandler = (event) => {
+    event.preventDefault();
+
+    let string = inputEl.value;
+
+    const vowelCounter = (string) => {
+        let vowelArr = ['a', 'e', 'i', 'o', 'u'];
+        let vowelsInStringArr = [];
+    
+        for (let i = 0; i < string.length; i++) {
+            for (let n = 0; n < vowelArr.length; n++) {
+                if (string[i] === vowelArr[n]) {
+                    vowelsInStringArr.push(string[i])
+                    console.log(string[i] + ' is a vowel')
+                }
             }
         }
+        console.log("'" + string + "'" + ' has ' + vowelsInStringArr.length + ' vowels in it.');
+        return "'" + string + "'" + ' has ' + vowelsInStringArr.length + ' vowels in it.';
     }
-    console.log(string + ' has ' + vowelsInStringArr.length + ' vowels in it.');
+
+    let result = vowelCounter(string);
+    console.log(result)
+    responseEl.textContent = result;
+    inputEl.value = '';
 }
 
-vowelCounter('thisisaword')
+buttonEl.addEventListener('click', clickHandler);
