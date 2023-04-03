0b342f19d7228361ea8e110e04fafd8be5646e24
ThereWolf <bdunham484@gmail.com>
2023-04-03 18:41:28 -0500

    pre class

---
+++ 05-Third-Party-APIs/04-preclass-drill/Mawnday/script.js
@@ -0,0 +1,16 @@
+var string = 'the quick brown fox jumps over the calm kitten quietly'
+
+const noRepeats = (string) => {
+    for (let i = 0; i < string.length; i++) {
+        let letter = string[i];
+        for (let n = 0; n < string.length; n++)  {
+            if (letter === string[n]) {
+                console.log(letter);
+            }
+        }
+    }
+
+
+}
+
+noRepeats(string);
