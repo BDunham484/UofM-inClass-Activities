3811eeaf60a1f6ff35d903e6f95597f05e3ce284
ThereWolf <bdunham484@gmail.com>
2023-04-05 18:06:40 -0500

    preclass

---
+++ 06-Server-Side-APIs/04-PreClass-Drill/Wednesday/script.js
@@ -0,0 +1,15 @@
+let numArr = [0,4,6,56,12,93,11,54,3];
+// let numArr = [1.5, 3, 2.5, 1]
+
+const averager = (arr) => {
+    let total = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        total += arr[i]
+    }
+
+    let average = total/arr.length 
+    console.log(average);
+}
+
+averager(numArr);
