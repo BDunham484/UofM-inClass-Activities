36b85f3b6391c86bcd97fd6285c08b0aaefab550
ThereWolf <bdunham484@gmail.com>
2023-04-19 18:22:18 -0500

    add 08 & 09

--- 07-Project-1/03-PreClass/wed/script.js
+++ 07-Project-1/03-PreClass/wed/script.js
@@ -1,7 +1,18 @@
 let intArr = [2,5,6,3,5];
 
 const duplicateHunter = (arr) => {
-    
+    var dups = {}
+    for (let i = 0; i < arr.length; i++) {
+        if(dups[arr[i]]) {
+            console.log(arr[i])
+            return arr[i]
+        } else {
+            dups[arr[i]] = true;
+            console.log(dups);
+        }
+    }
 }
+
+// consoidered faster than using array as dups.  would have to loop through each pass.  object just does a look up and saves time.  google object lookup 
 
 duplicateHunter(intArr);
