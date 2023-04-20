b4a86f8fcc5eec8ff4db42ce3a663d935ec7872e
ThereWolf <bdunham484@gmail.com>
2023-04-20 16:38:19 -0500

    classwork

--- 09-NodeJS/01-Activities/03-Ins_Arrow-Function/03-property-methods.js
+++ 09-NodeJS/01-Activities/03-Ins_Arrow-Function/03-property-methods.js
@@ -2,8 +2,10 @@
 var dog = {
   name: "Lassie",
   sound: "Woof!",
-  makeSound: () => console.log(this.sound),
-  readTag: () => console.log("The dog's tag reads: " + this.name + ".")
+  makeSound: function() { console.log(this.sound)},
+  // makeSound: () => console.log(this.sound),
+  readTag: function() {console.log("The dog's tag reads: " + this.name + ".")}
+  // readTag: () => console.log("The dog's tag reads: " + this.name + ".")
 };
 
 // Prints `undefined`
