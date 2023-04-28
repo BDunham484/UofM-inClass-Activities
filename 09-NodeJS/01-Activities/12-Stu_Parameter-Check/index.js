d66a2817d3e9f63790f0416930dc996012149570
ThereWolf <bdunham484@gmail.com>
2023-04-27 19:46:55 -0500

    class

--- 09-NodeJS/01-Activities/12-Stu_Parameter-Check/index.js
+++ 09-NodeJS/01-Activities/12-Stu_Parameter-Check/index.js
@@ -1,8 +1,4 @@
 const inputOne = process.argv[2];
 const inputTwo = process.argv[3];
 
-if (inputOne === inputTwo) {
-    console.log(true)
-} else {
-    console.log(false)
-}
+inputOne === inputTwo ? console.log(true) : console.log(false);
