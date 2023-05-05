e1e11086a9b488841fbaa632692d5f830bf84f04
ThereWolf <bdunham484@gmail.com>
2023-05-04 21:29:36 -0500

    class

--- 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
+++ 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
@@ -4,10 +4,11 @@ describe('BlogPost', () => {
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided is less than 2 characters long.
   describe('Author Length', () => {
     it('should throw an error when the authorName length is less than 2', () => {
-      const cb = () => new BlogPost('A');
-      const err = new Error('Author must be at least 2 characters long.')
+      // const cb = () => new BlogPost('A');
+      // const err = new Error('Author must be at least 2 characters long.')
 
-      expect(cb).toThrowError(err);
+      // expect(cb).toThrow(err);
+      expect(() => new BlogPost('A')).toThrow(new Error('Author must be at least 2 characters long.'));
     })
   })
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided does not only contain uppercase, lowercase, numbers, dashes, and underscores.
