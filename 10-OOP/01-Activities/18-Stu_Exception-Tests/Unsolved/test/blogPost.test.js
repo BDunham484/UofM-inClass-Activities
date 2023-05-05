134bdf3a2bebbcda3cbc4811ff9a2aa833263385
ThereWolf <bdunham484@gmail.com>
2023-05-04 21:23:08 -0500

    class

--- 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
+++ 10-OOP/01-Activities/18-Stu_Exception-Tests/Unsolved/test/blogPost.test.js
@@ -2,5 +2,16 @@ const BlogPost = require('../blogPost.js');
 
 describe('BlogPost', () => {
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided is less than 2 characters long.
+  describe('Author Length', () => {
+    it('should throw an error when the authorName length is less than 2', () => {
+      const cb = () => new BlogPost('A');
+      const err = new Error('Author must be at least 2 characters long.')
+
+      expect(cb).toThrowError(err);
+    })
+  })
   // TODO: Write a test that verifies that BlogPost throws an error when the authorName provided does not only contain uppercase, lowercase, numbers, dashes, and underscores.
+
 });
+
+
