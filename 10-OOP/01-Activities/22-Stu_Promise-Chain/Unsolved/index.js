802cb823cb07351bcca2fdf31b078dc31f562773
ThereWolf <bdunham484@gmail.com>
2023-05-08 15:31:43 -0500

    class

--- 10-OOP/01-Activities/22-Stu_Promise-Chain/Unsolved/index.js
+++ 10-OOP/01-Activities/22-Stu_Promise-Chain/Unsolved/index.js
@@ -7,11 +7,12 @@ const BlogPost = require('./lib/blogPost');
 readFile('./data/post.json', 'utf-8').then((json) => {
   console.log('Received data from post.json');
   // TODO: parse the json string and assign the resulting object to a variable
-  //
+    const { title, text, author, createdOn} = JSON.parse(json);
   // TODO: Use the BlogPost class to create a new BlogPost object and use its `render()` method to return an html string.
-  //
+  const newBlog = new BlogPost( title, text, author, createdOn)
   // TODO: Write the html to file
-  //
+  writeFile('./dist/index.html', newBlog.render())
 });
+
 
 // TODO: print a message in the console only after the html file is written.
