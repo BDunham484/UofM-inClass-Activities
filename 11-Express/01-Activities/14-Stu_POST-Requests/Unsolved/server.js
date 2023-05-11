a0b3f36d6273779d12744078c4375cf1b5e9b194
ThereWolf <bdunham484@gmail.com>
2023-05-11 17:49:54 -0500

    class

--- 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
+++ 11-Express/01-Activities/14-Stu_POST-Requests/Unsolved/server.js
@@ -7,18 +7,32 @@ const app = express();
 // TODO: Create a GET method for `/api/reviews` that logs when a user's request has been received
 app.get('/api/reviews', (req, res) => {
   // Your code here
+  res.json(`${req.method} request received`)
+  // Show the user agent information in the terminal
+  console.info(req.rawHeaders);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received`);
 });
 
 // TODO: Create a POST request for `/api/reviews` that logs when a user's request has been received
 // Your code here
+app.post('/api/reviews', (req, res) => {
+  res.json(`${req.method} request received`)
+})
 
 // TODO: Create a GET request for `api/upvotes` that logs when a user's request has been received
 app.get('/api/upvotes', (req, res) => {
   // Your code here
+  res.json(`${req.method} request received`)
 });
 
 // TODO: Create a POST request for `api/upvotes` that logs when a user's request has been received
 // Your code here
+app.post('/api/upvotes', (req, res) => {
+  // Your code here
+  res.json(`${req.method} request received`)
+});
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
