023b76add8d9bfb231e409c170479536c9c0d707
ThereWolf <bdunham484@gmail.com>
2023-05-15 17:20:33 -0500

    class

--- 11-Express/01-Activities/22-Stu_Modular-Routing/Unsolved/server.js
+++ 11-Express/01-Activities/22-Stu_Modular-Routing/Unsolved/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const util = require('util');
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+const api = require('./routes/index');
+// const fs = require('fs');
+// const util = require('util');
+// const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 // Helper method for generating unique ids
-const uuid = require('./helpers/uuid');
+// const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
@@ -14,6 +15,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('./api', api);
 
 app.use(express.static('public'));
 
