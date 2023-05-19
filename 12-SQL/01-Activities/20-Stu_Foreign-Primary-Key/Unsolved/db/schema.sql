9a2d55084be040fbd95e8253c9ad16a50888705f
ThereWolf <bdunham484@gmail.com>
2023-05-18 21:42:37 -0500

    class

--- 12-SQL/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -4,18 +4,18 @@ CREATE DATABASE data_db;
 
 USE data_db;
 
-CREATE TABLE (
+CREATE TABLE customers (
     id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE customer_order (
-    id INT,
-    customer_id INT NOT NULL,
+    id INT NOT NULL,
+    customer_id INT,
     order_details TEXT,
     FOREIGN KEY (customer_id)
-    REFERENCES instructors(id)
+    REFERENCES customers(id)
     ON DELETE SET NULL
-)
+);
