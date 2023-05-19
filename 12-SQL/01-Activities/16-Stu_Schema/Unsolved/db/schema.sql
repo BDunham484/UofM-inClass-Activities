a4f3e3bf6e25e06fdc831e43473556ece870496d
ThereWolf <bdunham484@gmail.com>
2023-05-18 19:19:54 -0500

    class

--- 12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,20 @@
 -- Write your schema here --
+DROP DATABASE IF EXISTS inventory_db;
+CREATE DATABASE inventory_db;
+
+USE inventory_db;
+
+CREATE TABLE products ( 
+    id INT NOT NULL,
+    product_name VARCHAR(30) NOT NULL,
+    category_name VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    n_stock BOOLEAN NOT NULL, 
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL 
+);
+
+CREATE TABLE categories (
+    id INT NOT NULL,
+    category_name VARCHAR(30),
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
--- 12-SQL/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -1,1 +1,21 @@
 -- Write your Schema Here -- 
+DROP DATABASE IF EXISTS  data_db;
+CREATE DATABASE data_db;
+
+USE data_db;
+
+CREATE TABLE (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id),
+);
+
+CREATE TABLE customer_order (
+    id INT,
+    customer_id INT NOT NULL,
+    order_details TEXT,
+    FOREIGN KEY (customer_id)
+    REFERENCES instructors(id)
+    ON DELETE SET NULL
+)
