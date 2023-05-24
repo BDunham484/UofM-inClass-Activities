c12b90c5d000798ed932586d56fb93e75a04cddd
ThereWolf <bdunham484@gmail.com>
2023-05-24 15:51:14 -0500

    class

--- 13-ORM/01-Activities/02-Stu_Sequelize-Setup/Unsolved/config/connection.js
+++ 13-ORM/01-Activities/02-Stu_Sequelize-Setup/Unsolved/config/connection.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
   'library_db',
   'root',
-  'myPassword',
+  process.env.PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
