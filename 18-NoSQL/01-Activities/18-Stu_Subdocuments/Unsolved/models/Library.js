53c54ed75d130d4979697e0bdbd3049cbb1c8555
ThereWolf <bdunham484@gmail.com>
2023-07-05 21:17:15 -0500

    classwork

--- 18-NoSQL/01-Activities/18-Stu_Subdocuments/Unsolved/models/Library.js
+++ 18-NoSQL/01-Activities/18-Stu_Subdocuments/Unsolved/models/Library.js
@@ -1,17 +1,56 @@
 const mongoose = require('mongoose');
 
 // TODO: Define a new schema named `bookSchema` for the subdocument
+const bookSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  author: {
+    type: String
+  },
+  price: {
+    type: Number
+  }
+});
 
 const librarySchema = new mongoose.Schema({
   name: { type: String, required: true },
   //
   // TODO: Add the `books` subdocument to the parent document as an array
+  books: [bookSchema],
   //
   lastAccessed: { type: Date, default: Date.now },
 });
 
 // TODO: Create a model named `Library`
+const Library = mongoose.model('Library', librarySchema);
 
 // TODO: Create a new instance of the model including the subdocuments
+const bookData = [
+  {
+    name: "The Tommyknockers",
+    author: "Stephen King",
+    price: 12.99
+  },
+  {
+    name: "1984",
+    author: "George Orwell",
+    price: 10.99
+  },
+  {
+    name: "American Psycho",
+    author: "Bret Easton Ellis",
+    price: 14.99
+  }
+]
+
+Library.create(
+  { name: 'Big Ol Book Place', books: bookData }, (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+    console.log(data);
+  }
+)
 
 module.exports = Library;
