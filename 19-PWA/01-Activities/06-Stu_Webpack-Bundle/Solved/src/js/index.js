d5487916d03e372d342d7c753a654ab2bcae5d4b
ThereWolf <bdunham484@gmail.com>
2023-07-10 20:09:03 -0500

    class

--- 19-PWA/01-Activities/06-Stu_Webpack-Bundle/Solved/src/js/index.js
+++ 19-PWA/01-Activities/06-Stu_Webpack-Bundle/Solved/src/js/index.js
@@ -3,6 +3,7 @@ import { headerClick } from './header';
 import '../css/style.css';
 // Import yellow robot image as Yellow
 import Yellow from '../images/yellow-robot.png';
+import Blue from '../images/blue-robot.png';
 
 document.getElementById('boxBtn').addEventListener('click', boxClick);
 document.getElementById('headerBtn').addEventListener('click', headerClick);
