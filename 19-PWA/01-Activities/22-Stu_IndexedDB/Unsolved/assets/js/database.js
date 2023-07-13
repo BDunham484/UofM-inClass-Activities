9bae840b2fb281f4782f17542e2629cc85cc1037
ThereWolf <bdunham484@gmail.com>
2023-07-13 18:57:15 -0500

    class

--- 19-PWA/01-Activities/22-Stu_IndexedDB/Unsolved/assets/js/database.js
+++ 19-PWA/01-Activities/22-Stu_IndexedDB/Unsolved/assets/js/database.js
@@ -3,15 +3,17 @@ import { openDB } from 'idb';
 const initdb = async () =>
 
 // TODO: Add a comment explaining what this method does
-
+//creates a new database named 'todos' which will be useing version 1 od the db
   openDB('todos', 1, {
     // TODO: Add a comment explaining the functionality of this method:
+    //add our database schema if it has not already been initialized
     upgrade(db) {
       if (db.objectStoreNames.contains('todos')) {
         console.log('todos database already exists');
         return;
       }
       // TODO: Add a comment explaining what we're doing with the object store:
+      // create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
       db.createObjectStore('todos', { keyPath: 'id', autoIncrement: true });
       console.log('todos database created');
     },
