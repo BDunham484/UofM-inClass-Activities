200b2db8f3cf5e86d5295223b0c5839ee4fee072
ThereWolf <bdunham484@gmail.com>
2023-07-17 19:17:05 -0500

    class

--- 20-React/01-Activities/00-practice-app/src/components/HelloReact.js
+++ 20-React/01-Activities/00-practice-app/src/components/HelloReact.js
@@ -4,8 +4,16 @@ import React from 'react';
 function HelloReact() {
   const text = 'some text';
 
+  const clickMe = () => {
+    console.log("I got clicked!")
+  }
+
   // TODO: Add a comment explaining what JSX is and the significance of the curly braces
-  return <p>Hello World! Here is {text}</p>;
+  return  <>
+  <p>Hello World! Here is {text}</p>
+  <button onClick={clickMe}>click me</button>
+  </>
+  
 }
 
 export default HelloReact;
