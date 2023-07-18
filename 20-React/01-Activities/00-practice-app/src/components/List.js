d7d1b169025ee29536612dbf0314760ef5be7039
ThereWolf <bdunham484@gmail.com>
2023-07-17 21:21:09 -0500

    class

---
+++ 20-React/01-Activities/00-practice-app/src/components/List.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function List({ groceries }) {
+  return (
+    <ul className="list-group">
+      {/* TODO: Use the `map` method to iterate over each grocery and return a `li` element that contains the item's name and has a unique key attribute */}
+      {groceries.map((item, index) => {
+        return (
+          // TODO: Your code here
+          <li key={index}>
+            {item.name} : Purchased: {item.purchased}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default List;
