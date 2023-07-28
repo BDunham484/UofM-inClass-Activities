df1715c65ce62b8e25253ee12f337af9c7bc922b
ThereWolf <bdunham484@gmail.com>
2023-07-27 21:13:57 -0500

    class

--- 21-MERN/01-Activities/26-Stu_Resolver-Context/Unsolved/client/src/pages/Profile.js
+++ 21-MERN/01-Activities/26-Stu_Resolver-Context/Unsolved/client/src/pages/Profile.js
@@ -12,6 +12,7 @@ import Auth from '../utils/auth';
 const Profile = () => {
   const { username: userParam } = useParams();
 
+
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
