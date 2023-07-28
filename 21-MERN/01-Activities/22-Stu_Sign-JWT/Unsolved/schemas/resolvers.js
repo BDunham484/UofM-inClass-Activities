53039a85ca45443aaf6f0d699b3f403c6bf66d1e
ThereWolf <bdunham484@gmail.com>
2023-07-27 20:07:04 -0500

    class

--- 21-MERN/01-Activities/22-Stu_Sign-JWT/Unsolved/schemas/resolvers.js
+++ 21-MERN/01-Activities/22-Stu_Sign-JWT/Unsolved/schemas/resolvers.js
@@ -22,26 +22,30 @@ const resolvers = {
   Mutation: {
     // TODO: Add comments to each line of code below to describe the functionality below
     addUser: async (parent, args) => {
+      //first we create the user
       const user = await User.create(args);
+      //immediately sign a sjon web token an log the user in after they are created
       const token = signToken(user);
+      //return an auth object that consits of the signed toke and user's information
 
       return { token, user };
     },
     // TODO: Add comments to each line of code below to describe the functionality below
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
-
+//if there is no user with that email address, return an authentication error
       if (!user) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
+//if there is a user found, execute the 'iscorrectpasswor'instance method and check if the correct password was provided
       const correctPw = await user.isCorrectPassword(password);
-
+//if the password is incorrect, return an authentiaction error stating so
       if (!correctPw) {
         throw new AuthenticationError('Incorrect credentials');
       }
-
+//if email and password are correct, sign user into the application with a jwt
       const token = signToken(user);
+      //reutrn an auth object that consists of the signed toke and user's information
       return { token, user };
     },
     addThought: async (parent, { thoughtText, thoughtAuthor }) => {
